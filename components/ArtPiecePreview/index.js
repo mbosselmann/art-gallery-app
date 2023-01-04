ffa5c65ec093cb04f5bd1553b4e639e5d6bdba4d
Mareike Boßelmann <77329824+mbosselmann@users.noreply.github.com>
2023-01-04 22:28:01 +0100

    User Story 1 (#1)
    * Create components
    
    * Change next config
    
    * Install SWR
    
    * Update components structure
    
    * Add tests for components

---
+++ components/ArtPiecePreview/index.js
@@ -0,0 +1,40 @@
+import Image from "next/image.js";
+import styled from "styled-components";
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const Figure = styled.figure`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+`;
+
+const Caption = styled.figcaption`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: black;
+  color: white;
+  padding: 0.5rem 0.3rem;
+`;
+
+const StyledImage = styled(Image)`
+  object-fit: cover;
+`;
+
+export default function ArtPiecePreview({ title, image, artist }) {
+  return (
+    <Figure>
+      <ImageContainer>
+        <StyledImage src={image} fill alt={`${artist}: ${title}`} />
+      </ImageContainer>
+      <Caption>{`${artist}: ${title}`}</Caption>
+    </Figure>
+  );
+}
