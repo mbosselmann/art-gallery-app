1d7abf64ce335b761e26f5720b794e3d0b5d402a
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-07 00:14:33 +0100

    Create favorites page

---
+++ pages/favorites/index.js
@@ -0,0 +1,21 @@
+import ArtPieces from "../../components/ArtPieces";
+
+export default function FavoritesPage({
+  pieces,
+  artPiecesInfo,
+  onArtPiecesInfo,
+  onToggleFavorite,
+}) {
+  const favorites = pieces.filter((piece) =>
+    artPiecesInfo.some((artPiece) => artPiece.slug === piece.slug)
+  );
+
+  return (
+    <ArtPieces
+      pieces={favorites}
+      onArtPiecesInfo={onArtPiecesInfo}
+      artPiecesInfo={artPiecesInfo}
+      onToggleFavorite={onToggleFavorite}
+    />
+  );
+}
