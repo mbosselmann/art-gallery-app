8a9b910a40e63ee605f221760c0e2a17b5db8e13
Mareike Boßelmann <77329824+mbosselmann@users.noreply.github.com>
2023-01-07 00:16:23 +0100

    User Story 6 (#6)
    * Create favorites page
    
    * Update navigation component and test
    
    * Update favorite button test

--- components/Navigation/Navigation.test.js
+++ components/Navigation/Navigation.test.js
@@ -1,12 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Navigation from "./index.js";
 
-test("renders two links, one labeled 'Spotlight' and the other 'Pieces'", () => {
+test("renders three links", () => {
   render(<Navigation />);
 
   const spotlightLink = screen.getByRole("link", { name: "Spotlight" });
   const artPiecesLink = screen.getByRole("link", { name: "Art Pieces" });
+  const favoritesLink = screen.getByRole("link", { name: "Favorites" });
 
   expect(spotlightLink).toBeInTheDocument();
   expect(artPiecesLink).toBeInTheDocument();
+  expect(favoritesLink).toBeInTheDocument();
 });
