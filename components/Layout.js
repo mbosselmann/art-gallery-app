9cce88cfc98a37b8a9210f4735050561658f577b
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-07 20:14:12 +0100

    Update layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,28 @@
 import Navigation from "./Navigation/index.js";
+import styled from "styled-components";
+import { useEffect } from "react";
+import { useRouter } from "next/router.js";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 5rem auto 4rem;
+`;
+
+const Main = styled.main`
+  overflow-y: scroll;
+`;
+
+const Headline = styled.h1`
+  text-align: center;
+`;
 
 export default function Layout({ children }) {
   return (
-    <>
-      {children}
+    <Wrapper>
+      <Headline>Art Gallery</Headline>
+      <Main>{children}</Main>
       <Navigation />
-    </>
+    </Wrapper>
   );
 }
