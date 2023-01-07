75230c1811727e2ebb241d626b8249dd2642e558
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-07 20:14:59 +0100

    Update navigation component

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,19 +1,53 @@
-import Link from "next/link.js";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import styled from "styled-components";
+
+const List = styled.ul`
+  padding-left: 0;
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  gap: 0.8rem;
+  height: 100%;
+  margin: 0;
+  align-items: center;
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  padding: 0 0.3rem;
+  color: ${({ active }) => (active ? "lightcoral" : "black")};
+  font-weight: bold;
+  &:hover {
+    border-bottom: 5px dotted lightcoral;
+  }
+`;
 
 export default function Navigation() {
+  const router = useRouter();
+
   return (
     <nav>
-      <ul>
+      <List role="list">
         <li>
-          <Link href="/">Spotlight</Link>
+          <NavLink href="/" active={router.pathname === "/"}>
+            Spotlight
+          </NavLink>
         </li>
         <li>
-          <Link href="/art-pieces">Art Pieces</Link>
+          <NavLink
+            href="/art-pieces"
+            active={router.pathname === "/art-pieces"}
+          >
+            Art Pieces
+          </NavLink>
         </li>
         <li>
-          <Link href="/favorites">Favorites</Link>
+          <NavLink href="/favorites" active={router.pathname === "/favorites"}>
+            Favorites
+          </NavLink>
         </li>
-      </ul>
+      </List>
     </nav>
   );
 }
